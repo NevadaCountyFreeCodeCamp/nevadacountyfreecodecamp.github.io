9d279700b14c9590b66e0b34c99c1f33bc15c752
Andreas Groos <andreas.groos1@gmail.com>
2017-06-01 21:17:38 -0700

    Both menus technically work, mobile uses $.slideUp/Down

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   $('#hamburger').click(function() {
-    $('.navbar').toggleClass('open-menu');
-    $('.navbar').slideToggle('slow');
+    if ($('.navbar').hasClass('open-menu')) {
+      // This is meant to prevent the <ul> from dissapearing at oncce on click, but doesn't work
+      // Chaining doesn't work not setTimeout
+      $('.navbar').slideUp('slow');
+      setTimeout($('.navbar').removeClass('open-menu'), 1500);
+    } else {
+      $('.navbar').addClass('open-menu').slideDown('slow');
+    }
+
+  });
+  $(window).resize(function() {
+    $('.navbar').removeClass('open-menu');
+    var width = $(window).width();
+    if (width > 950) {
+      $('.navbar').css('display', 'block');
+    } else {
+      $('.navbar').css('display', 'none');
+    }
   });
 });
+
