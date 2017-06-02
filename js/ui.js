9d32dc2efbcb9b10dad8a829e3b678a281fcd38c
Andreas Groos <andreas.groos1@gmail.com>
2017-06-02 14:07:27 -0700

    sidemenu is fine, added social media icons, more SCSS variables....

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,7 @@ function openNav() {
   $('.sidenav').addClass('sidenav-open');
   $('#main').addClass('move-right');
   $('#hamburger').hide();
+  // close on ESC-key
   document.onkeydown = function(e) {
     e = e || window.event;
     console.log(e.keyCode);
@@ -9,9 +10,13 @@ function openNav() {
       closeNav();
     }
   };
+  $('#main').click(function() {
+    closeNav();
+  });
 }
 
 function closeNav() {
+  // Do I need to remove the onkeydown event from above?
   $('.sidenav').removeClass('sidenav-open');
   $('#main').removeClass('move-right');
   $('#hamburger').show();
