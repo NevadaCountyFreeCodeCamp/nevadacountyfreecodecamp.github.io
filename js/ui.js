41f0929efa4f71a1e6ff3f240bde0619d8b62a2a
Andreas Groos <andreas.groos1@gmail.com>
2017-07-12 12:44:17 -0700

    rotating bg-images

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,11 @@
+// Name of bg-images
+var bgImages = ['1.jpg', '2.jpg', '3.jpg'];
+// img-directory
+var baseURL = './img/';
+// Interval for background image change
+var secs = 10;
+
+
 function openNav() {
   $('.sidenav').addClass('sidenav-open');
   $('#main').addClass('move-right');
@@ -21,6 +29,28 @@ function closeNav() {
   $('.sidenav').removeClass('sidenav-open');
   $('#main').removeClass('move-right');
   $('#hamburger').show();
-
 }
 
+// ROATING BG-IMAGES
+
+function changeBgImage() {
+  window.clearTimeout();
+  var k = 0;
+  for (i = 0; i < bgImages.length; i++) {
+    setTimeout(function() {
+      console.log('image change ', i, ' url ', baseURL + bgImages[k]);
+      // FIXME: Can't seem to use $, even with $(document).ready, works fine with plain JS though
+      document.getElementById('banner').style.background = 'url(' + baseURL + bgImages[k] + ') no-repeat ';
+      document.getElementById('banner').style.backgroundSize = "cover";
+      if ((k + 1) === bgImages.length) {
+        setTimeout(function() {
+          changeBgImage();
+        }, (secs * 1000));
+      } else {
+        k++;
+      }
+    }, (secs * 1000) * i);
+  }
+}
+
+changeBgImage();
